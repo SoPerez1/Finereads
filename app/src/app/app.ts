4efd92003bc84a86d3adf34229fcec1737ec4753
Tu Nombrck <perezsofi149@gmail.com>
2025-10-05 14:02:47 -0300

    grids

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -6,7 +6,7 @@ import { Start } from "./components/start/start/start";
   selector: 'app-root',
   imports: [RouterOutlet, Start],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('finereads');
