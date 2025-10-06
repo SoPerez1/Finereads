e4186207417437d03281ffe65e4588c36f25ec9a
Tu Nombrck <perezsofi149@gmail.com>
2025-10-06 13:12:45 -0300

    grid bento and text

--- app/src/app/components/start/start/start.ts
+++ app/src/app/components/start/start/start.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-start',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './start.html',
   styleUrl: './start.css'
 })
 export class Start {
-title: string = "Scriba";
-description: string =""
+bento1: string = "Algunas historias te elijen a vos";
+bento2: string ="Entre las sombras de las páginas laten mundos enteros, esperando ser descubiertos."
+printThis : boolean = false;
+
+  messageOn(){
+    this.printThis= true;
+  }
+
+  messageOff(){
+    this.printThis = false;
+  }
 }
