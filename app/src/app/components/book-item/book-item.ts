76f903854d4948cad27058f4c7bf21b993672b44
Tu Nombrck <perezsofi149@gmail.com>
2025-10-13 14:29:54 -0300

    add hijo component

--- app/src/app/components/book-item/book-item.ts
+++ app/src/app/components/book-item/book-item.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 @Component({
   selector: 'app-book-item',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book-item.html',
-  styleUrl: './book-item.css'
+  styleUrls: ['./book-item.css']
 })
 export class BookItem {
-
+  @Input() book: any;
+  @Output() onSelect: EventEmitter<string> = new EventEmitter<string>();
+  selectBook(): void {
+    this.onSelect.emit(this.book.title);
+  }
 }
